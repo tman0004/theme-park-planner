8d146bb7b2d08a4971da66d4fa2163ad7923110c
Myondusk <khauvi@uci.edu>
2022-04-26 01:29:16 -0700

    Started making selection page UI more uniform

--- Frontend/js/start_plan.js
+++ Frontend/js/start_plan.js
@@ -1,10 +1,8 @@
 
 window.onload = function() {
     var button = document.getElementById("start_plan_button")
-    button.onclick = planClick;
-}
-
-function planClick () {
-    window.open("https://github.com/RyanLiu2015/theme-park-planner#planned-features")
+    button.onclick = planClick = function() {
+        location.href = "./selection.html";
+    }
 }
 
