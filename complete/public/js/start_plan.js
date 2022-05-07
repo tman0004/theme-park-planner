a3a649eb1b579e03ec314d7d7f0e3fb018e92ce9
Aditya Kuppili <adityakuppili@adityas-mbp-2.lan>
2022-05-07 15:36:47 -0700

    fully connected

---
+++ complete/public/js/start_plan.js
@@ -0,0 +1,8 @@
+
+window.onload = function() {
+    var button = document.getElementById("start_plan_button")
+    button.onclick = planClick = function() {
+        location.href = "./selection.html";
+    }
+}
+
